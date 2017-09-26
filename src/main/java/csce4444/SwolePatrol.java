a03c93945e6e61e1d02e30d25eb2bffce1c907de
Unknown <jacob.hanson1010@gmail.com>
2017-09-26 14:54:36 -0500

    switching to spring boot thymeleaf to serve html

--- src/main/java/csce4444/SwolePatrol.java
+++ src/main/java/csce4444/SwolePatrol.java
@@ -1,11 +1,12 @@
 package csce4444;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 // This class acts as the main web controller. When a web request comes in, it is handled by the appropriate
 // RequestMapping method. Starts on port 8080 by default.
@@ -21,9 +22,10 @@ public class SwolePatrol {
 	// These are the request mappings for handling web requests. 
 	
 	@RequestMapping("/")
-	@ResponseBody
-	String index() {
-		return "Hello World! -> " + dataCollector.getSwipesForDate(null);
+	public String index(Map<String, Object> model) {
+		// replace variables in the html file
+		model.put("swipes", ""+(Math.random()*100));
+		return "index"; // this is the name of the html file to return
 	}
 
 	/**
